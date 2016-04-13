3d4421d5614632520c1566f2ba98d1ca0a789a54
Daniel Hunninghake <daniel@drip.fm>
2016-04-13 14:00:18 -0400

    bring back nodes on mobile, slight refactor

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 
   var handleCanvasIcons = {
     icons: [],
-    xStart: 104,
+    xStart: -22,
     yStart: 93,
+    honeycombWidth: 126,
+    honeycombHeight: 220,
     populateIcons: function () {
       var self = this;
       $('g[id^="cw"]').each(function () {
@@ -14,36 +16,36 @@
       return Math.floor(Math.random()*(max-min+1)+min);
     },
     staggerPosition: function (x, y) {
-      var xCap = Math.floor($('#cw-icons').width() / 126) - 1;
-      var yCap = Math.floor($('#cw-icons').height() / 220) - 1;
-      var coordinates = {
-        x: x + 126 * this.randomizer(0, xCap),
-        y: y + 220 * this.randomizer(0, yCap)
+      var $canvas = $('#cw-icons');
+      var xCap = Math.floor($canvas.width() / this.honeycombWidth);
+      var yCap = Math.floor($canvas.height() / this.honeycombHeight);
+      var newCoordinates = {
+        x: x + this.honeycombWidth * this.randomizer(0, xCap),
+        y: y + this.honeycombHeight * this.randomizer(0, yCap)
       }
-      return coordinates;
+      return newCoordinates;
     },
     setPosition: function (index, icon) {
       var delay = this.randomizer(0,700);
-      if (index < this.icons.length / 2) {
-        var coords = this.staggerPosition(this.xStart, this.yStart);
-      } else if (index >= this.icons.length / 2) {
-        var coords = this.staggerPosition(this.xStart+63, this.yStart+110);
+      if (index < this.icons.length/2) {
+        var coordinates = this.staggerPosition(this.xStart, this.yStart);
+      } else if (index >= this.icons.length/2) {
+        var coordinates = this.staggerPosition(this.xStart+63, this.yStart+110);
       }
 
       $(icon).css({
-        'transform':'translate('+coords.x+'px,'+coords.y+'px)',
+        'transform':'translate('+coordinates.x+'px,'+coordinates.y+'px)',
         'transition': 'opacity 1s ease '+delay+'ms'
       });
+
       $(icon).removeClass('hide');
     },
     init: function () {
       var self = this;
-
       this.populateIcons();
       $.each(self.icons, function (i, icon) {
         self.setPosition(i, icon);
       });
-
     }
   };
 
